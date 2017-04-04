5a758252bdc71aed21ffa3aa370734bd64b5e9ce
kapllanis <stephkap@vt.edu>
2017-04-04 08:48:09 -0400

    New Images

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++
@@ -1,7 +0,0 @@
-//List of files which are indexed.
-fl = new Array();
-fl["0"]= "introduction.html";
-fl["1"]= "task-bloodymary.html";
-fl["2"]= "task-breakfastsandwich.html";
-fl["3"]= "task-fruitsalad.html";
-var doStem = false;searchLoaded = true;
